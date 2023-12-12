35cf617759f490061929037a8d1b15e2f50dfbee
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-12-12 17:17:30 +0530

    chat bot created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
-import 'package:iktbot/home.dart';
+import 'package:ikchatbot/ikchatbot.dart';
+import 'package:iktbot/chatqueries.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +12,36 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ChatBot()
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('ikChatBot Example'),
+      ),
+      body: ikchatbot(config: chatBotConfig),
     );
   }
 }
